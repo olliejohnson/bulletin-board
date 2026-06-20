9aecb508d589088c3fddbd323c40969eb06b5aa8
olliejohnson <oliver.joseph.johnson@gmail.com>
2026-06-20 18:04:24 -0400

    this should fix the warning

--- apps/web/app/log-out/route.ts
+++ apps/web/app/log-out/route.ts
@@ -3,6 +3,7 @@ import { headers } from "next/headers"
 import { redirect } from "next/navigation"
 import { NextRequest } from "next/server"
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(_request: NextRequest) {
   auth.api.signOut({
     headers: await headers(),
