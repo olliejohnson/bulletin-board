9eb68231ca553dc08a0f7a4f607337a2848c04ab
Jake Johnson <jacob.errol@gmail.com>
2026-06-20 17:10:27 -0400

    Revert "Merge pull request #4 from olliejohnson/jj/refactor-login-signup"
    This reverts commit 00f9d3872322e0f5736991540ebf90cb2eae7c94, reversing
    changes made to cb0bad5ee7f6cf89b7cb5b887b4f9d6c8c2d5371.

--- apps/web/app/sign-in/page.tsx
+++ apps/web/app/sign-in/page.tsx
@@ -1,8 +1,8 @@
 "use client"
-import { AuthLayout } from "@/components/auth-layout"
 import { LoginForm } from "@/components/login-form"
 import { signInSchema } from "@/app/sign-in/schema"
 import { authClient } from "@/lib/auth-client"
+import { IconFoldersFilled } from "@tabler/icons-react"
 import { redirect } from "next/navigation"
 import { SubmitEvent, useState } from "react"
 
@@ -43,8 +43,29 @@ export default function Page() {
   }
 
   return (
-    <AuthLayout>
-      <LoginForm onSubmit={signin} error={error} />
-    </AuthLayout>
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <IconFoldersFilled className="size-4" />
+            </div>
+            Acme Inc.
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm onSubmit={signin} error={error} />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src="/placeholder.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
   )
 }
