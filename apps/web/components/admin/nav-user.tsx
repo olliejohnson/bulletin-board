c25a81040b4cf3c779930bec36081555ffc0acd8
olliejohnson <oliver+github@oliverj.io>
2026-06-23 18:18:42 -0400

    Added menu to board to access admin ui

--- apps/web/components/admin/nav-user.tsx
+++ apps/web/components/admin/nav-user.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { CurrentUser } from "@/lib/session"
 import {
   IconCreditCard,
   IconDotsVertical,
@@ -28,15 +29,7 @@ import {
   useSidebar,
 } from "@workspace/ui/components/sidebar"
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
-}) {
+export function NavUser({ user }: { user: CurrentUser }) {
   const { isMobile } = useSidebar()
 
   return (
@@ -49,7 +42,7 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg grayscale">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarImage src={user.image ?? ""} alt={user.name} />
                 <AvatarFallback className="rounded-lg">
                   {user.name.split(" ")[0]?.charAt(0)?.toUpperCase() ||
                     "" + user.name.split(" ")[1]?.charAt(0).toUpperCase() ||
@@ -74,7 +67,7 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarImage src={user.image ?? ""} alt={user.name} />
                   <AvatarFallback className="rounded-lg">
                     {user.name.split(" ")[0]?.charAt(0)?.toUpperCase() ||
                       "" + user.name.split(" ")[1]?.charAt(0).toUpperCase() ||
